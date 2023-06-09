2b51d7ee3a52ea5979ede18e1deba56656b3adfa
ann1205 <gannanychyporchuk@gmail.com>
2023-06-09 22:00:06 +0300

    dashbord homework

--- src/route/index.js
+++ src/route/index.js
@@ -35,6 +35,22 @@ router.get('/slack', function (req, res) {
 // ================================================================
 
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', {
+
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
